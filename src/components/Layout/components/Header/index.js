56db6677fbf739117767062791d842e091878e79
Your Name <you@example.com>
2023-07-16 07:24:53 +0700

    dựng header phần 1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,36 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+   faCircleXmark,
+   faMagnifyingGlass,
+   faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 const Header = () => {
    return (
       <header className={cx('wrapper')}>
-         <div className={cx('inner')}></div>
+         <div className={cx('inner')}>
+            <div className={cx('logo')}>
+               <img src={images.logo} />
+            </div>
+            <div className={cx('search')}>
+               <input placeholder="Tim kiem" />
+               <button className={cx('clear-btn')}>
+                  <FontAwesomeIcon icon={faCircleXmark} />
+               </button>
+               <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+               <button className={cx('search-btn')}>
+                  <FontAwesomeIcon icon={faMagnifyingGlass} />
+               </button>
+            </div>
+            <div className={cx('actions')}>
+               <button>Đăng nhập</button>
+               <button>option</button>
+            </div>
+         </div>
       </header>
    );
 };
